701353580892e06f92e81339bf09f9f28e7ea9dc
oliv245 <117729391+oliv245@users.noreply.github.com>
2022-11-23 12:37:47 +0100

    Commit 1 _ Wep App

---
+++ functions.py
@@ -0,0 +1,18 @@
+def get_todos(filepath="todos.txt"):
+    """ Read a text file and return the list of
+    to do items.
+    """
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg,filepath="todos.txt") :
+    """ Write the to do item in text file.
+    """
+    with open(filepath, 'w') as file_local:
+        file_local.writelines(todos_arg)
+    return
+
+if __name__ == "__main__" :
+    print("Hello from function")
